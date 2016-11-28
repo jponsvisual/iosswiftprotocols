4fef832953015ca8817f30f87fd45d683b789350
Joan Pons <jpons@visual-engin.com>
2016-11-28 12:00:10 +0100

    Creación del proyecto

---
+++ swiftProtocolsAndGenerics/swiftProtocolsAndGenerics.h
@@ -0,0 +1,19 @@
+//
+//  swiftProtocolsAndGenerics.h
+//  swiftProtocolsAndGenerics
+//
+//  Created by Joan Pons on 28/11/16.
+//  Copyright © 2016 Joan Pons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftProtocolsAndGenerics.
+FOUNDATION_EXPORT double swiftProtocolsAndGenericsVersionNumber;
+
+//! Project version string for swiftProtocolsAndGenerics.
+FOUNDATION_EXPORT const unsigned char swiftProtocolsAndGenericsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftProtocolsAndGenerics/PublicHeader.h>
+
+
